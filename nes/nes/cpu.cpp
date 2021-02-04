1eb5d187e67f9c7aecbe338ae83bf8a751d58d84
yashpatel5400 <ypatel5400@gmail.com>
2021-02-03 21:36:59 -0500

    Added very basic implementations

--- nes/nes/cpu.cpp
+++ nes/nes/cpu.cpp
@@ -75,6 +75,29 @@ bool CPU::srC() {
 
 /************************************************************************************
 
+Helper methods for memory access that are used repeatedly throughout the code:
+
+Address Modes:
+
+A		....	Accumulator	 	        OPC A	 	    operand is AC (implied single byte instruction)
+abs		....	absolute	 	        OPC $LLHH	 	operand is address $HHLL *
+abs,X	....	absolute, X-indexed	 	OPC $LLHH,X	 	operand is address; effective address is address incremented by X with carry **
+abs,Y	....	absolute, Y-indexed	 	OPC $LLHH,Y	 	operand is address; effective address is address incremented by Y with carry **
+#		....	immediate	 	        OPC #$BB	 	operand is byte BB
+impl	....	implied	 	            OPC	 	        operand implied
+ind		....	indirect	 	        OPC ($LLHH)	 	operand is address; effective address is contents of word at address: C.w($HHLL)
+X,ind	....	X-indexed, indirect	 	OPC ($LL,X)	 	operand is zeropage address; effective address is word in (LL + X, LL + X + 1), inc. without carry: C.w($00LL + X)
+ind,Y	....	indirect, Y-indexed	 	OPC ($LL),Y	 	operand is zeropage address; effective address is word in (LL, LL + 1) incremented by Y with carry: C.w($00LL) + Y
+rel		....	relative	 	        OPC $BB	 	    branch target is PC + signed offset BB ***
+zpg		....	zeropage	 	        OPC $LL	 	    operand is zeropage address (hi-byte is zero, address = $00LL)
+zpg,X	....	zeropage, X-indexed	 	OPC $LL,X	 	operand is zeropage address; effective address is address incremented by X without carry **
+zpg,Y	....	zeropage, Y-indexed	 	OPC $LL,Y	 	operand is zeropage address; effective address is address incremented by Y without carry **
+
+
+*************************************************************************************/
+
+/************************************************************************************
+
 ADC  Add Memory to Accumulator with Carry
 
      A + M + C -> A, C                N Z C I D V
@@ -93,19 +116,47 @@ ADC  Add Memory to Accumulator with Carry
 
 *************************************************************************************/
 void CPU::ADC(uint16_t opcode) { //add with carry
+    uint8_t oper = memory[rpc++];
+
     switch (opcode) {
     case 0x69: {
-        uint16_t arg = readShort(rpc);
-        rpc += 2;
+        rac += oper;
         break;
     }
-    case 0x65: break;
-    case 0x75: break;
-    case 0x6D: break;
-    case 0x7D: break;
-    case 0x79: break;
-    case 0x61: break;
-    case 0x71: break;
+    case 0x65: {
+        rac += memory[oper];
+        break;
+    }
+    case 0x75: {
+        rac += memory[oper + rx];
+        break;
+    }
+    case 0x6D: {
+        rpc++;
+        uint16_t operand = readShort(rpc - 2);
+        rac += memory[operand];
+        break;
+    }
+    case 0x7D: {
+        rpc++;
+        uint16_t operand = readShort(rpc - 2);
+        rac += memory[operand + rx];
+        break;
+    }
+    case 0x79: {
+        rpc++;
+        uint16_t operand = readShort(rpc - 2);
+        rac += memory[operand + ry];
+        break;
+    }
+    case 0x61: {
+        rac += memory[oper];
+        break;
+    }
+    case 0x71: {
+        rac += memory[oper];
+        break;
+    }
     default: throw std::runtime_error("Incorrect dispatch: " + opcode);
     }
 }
@@ -742,7 +793,12 @@ LDA  Load Accumulator with Memory
 *************************************************************************************/
 void CPU::LDA(uint16_t opcode) { //load accumulator
     switch (opcode) {
-    case 0xA9: break;
+    case 0xA9: {
+        uint16_t arg = readShort(rpc);
+        rpc += 2;
+        rac += memory[arg];
+        break;
+    }
     case 0xA5: break;
     case 0xB5: break;
     case 0xAD: break;
